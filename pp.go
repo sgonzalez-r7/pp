32117cbe4ce4fbb750b1f39fad72d6c7ce9a89ef
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2020-02-21 09:27:46 -0600

    Add Fprintjson helper that delegates to Fprintjson method

--- pp.go
+++ pp.go
@@ -35,6 +35,11 @@ func (f *JsonFormatter) Printjson(v interface{}) {
         f.Fprintjson(os.Stderr, v)
 }
 
+func Fprintjson(w io.Writer, v interface{}) {
+        f := NewJsonFormatter()
+        f.Fprintjson(w, v)
+}
+
 func Printjson(v interface{}) {
 	enc := json.NewEncoder(os.Stderr)
 	enc.SetIndent("", "    ")
