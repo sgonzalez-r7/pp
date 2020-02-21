d3051441cbeaae1d021c72e7028b2dc2b17e714b
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2020-02-21 09:22:18 -0600

    Add Json Formatter and associated print methods

--- pp.go
+++ pp.go
@@ -6,6 +6,35 @@ import (
 	"os"
 )
 
+type JsonFormatter struct {
+        Prefix string
+        Indent string
+}
+
+const (
+        jsonFormatterPrefixDefault = ""     // empty string
+        jsonFormatterIndentDefault = "    " // 4 spaces
+)
+
+func NewJsonFormatter(opts... jsonFormatterOption) *JsonFormatter {
+        return JsonFormatter{
+                Prefix: jsonFormatterPrefixDefault,
+                Indent: jsonFormatterIndentDefault,
+        }
+}
+
+func (f *JsonFormatter) Fprintjson(w io.Writer, v interface{}) {
+        enc := json.NewEncoder(w)
+        enc.SetIndent(f.Prefix, f.Indent)
+        if err := enc.Encode(v); err != nil {
+                fmt.Fprintf(os.Stderr, "pp: %s\n", err.Error())
+        }
+}
+
+func (f *JsonFormatter) Printjson(v interface{}) {
+        f.Fprintjson(os.Stderr, v)
+}
+
 func Printjson(v interface{}) {
 	enc := json.NewEncoder(os.Stderr)
 	enc.SetIndent("", "    ")
