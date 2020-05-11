f9da59ac521229de5e8fd96f7b7635a78d1af6d7
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2020-05-11 08:04:37 -0500

    Add spew

--- pp.go
+++ pp.go
@@ -3,6 +3,7 @@ package pp
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"io"
 	"os"
 )
@@ -63,4 +64,51 @@ func Fprintjson(w io.Writer, v interface{}) {
 
 func Printjson(v interface{}) {
 	Fprintjson(os.Stderr, v)
+}
+
+//
+// Spew
+//
+
+var (
+	scs spew.ConfigState = spew.ConfigState{
+		Indent: "    ",
+		DisableMethods: true,
+		DisablePointerAddresses: true,
+	}
+)
+
+type KVPair struct {
+	K string
+	V interface{}
+}
+
+func FspewDump(w io.Writer, args ...interface{}) {
+	if len(args)%2 != 0 {
+		msg := fmt.Sprintf("pp.FspewDump: kvpairs must be even. got %d", len(args))
+		scs.Fdump(w, msg)
+		return
+	}
+
+	kvps := make([]KVPair, len(args)/2)
+	for i := 0; i < len(kvps); i++ {
+		kvps[i] = KVPair{K: args[2*i].(string), V: args[2*i+1]}
+	}
+
+	scs.Fdump(w, kvps)
+}
+
+func SpewDump(args ...interface{}) {
+	if len(args)%2 != 0 {
+		msg := fmt.Sprintf("pp.FspewDump: kvpairs must be even. got %d", len(args))
+		scs.Dump(msg)
+		return
+	}
+
+	kvps := make([]KVPair, len(args)/2)
+	for i := 0; i < len(kvps); i++ {
+		kvps[i] = KVPair{K: args[2*i].(string), V: args[2*i+1]}
+	}
+
+	scs.Dump(kvps)
 }
